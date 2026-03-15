6807f6bf14e4c2848feec3fe9293bf4783d06e4c
VenkoCode <venkocode@gmail.com>
2026-03-15 16:46:47 +0100

    Fix formatting issues in server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 import Fastify from "fastify"
+
 import { foodRoutes } from "./src/routes/foodRoutes.js"
 import { coachRoutes } from "./src/routes/coachRoutes.js"
 
 const fastify = Fastify({
-  logger: true
-  bodyLimit: 10 * 1024 * 1024 // 10 MB
+  logger: true,
+  bodyLimit: 10 * 1024 * 1024
 })
 
 fastify.get("/", async () => {
@@ -21,7 +22,7 @@ const start = async () => {
       host: "0.0.0.0"
     })
 
-    console.log("Server started on port", process.env.PORT || 3000)
+    console.log("Server started")
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
